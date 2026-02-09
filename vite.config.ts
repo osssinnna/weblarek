07b76cb257425cc5b6b08f53c377f162fb08ecb0
Darya <127752063+osssinnna@users.noreply.github.com>
2026-02-09 13:27:22 +0300

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/weblarek/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "index.html",
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
